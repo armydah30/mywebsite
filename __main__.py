1e78eb6a33311128210ba2084cc2a1ac0b497951
armydah30 <hameedatomoine@gmail.com>
2018-05-11 15:38:31 +0200

    update main

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import os
 from mysite.app import app
 
-if __name__ == '__main__': #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
-	app.run(debug=True)  #Start listening
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
+	app.run(host='0.0.0.0', port=port)#Start listening
